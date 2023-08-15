a8ad4986d44af08581f0bd2f7f1a7231cd4dfbda
real-folkblues <marenicolor@gmail.com>
2023-08-14 21:59:14 -0400

    task 4 buttons and callback

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import satData from './satData';
 
-const Buttons = () =>
-{
-  filterByType, setSat, displaySats
+const Buttons = ({filterByType, setSat, displaySats }) => 
   return (
-    <div>
-      <button>{sat} Orbit</button>
-      <button>All Orbits</button>
-      </div>
-  );
+  <div>
+    {displaySats.map((sat,id) => (
+      <button onClick={() => filterByType(sat)} key={id}>
+        {sat} Orbit
+      </button>
+    ))}
+    <button>All Orbits</button>
+  </div>
+);
 };
 
 export default Buttons;
