bd41cbdd245fbf5a14c60d0be42a2b44d4b99551
real-folkblues <marenicolor@gmail.com>
2023-08-14 21:10:28 -0400

    added const to app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import Buttons from "./components/Buttons";
 import Table from "./components/Table";
 import Banner from "./components/Banner";
+import useState from "react";
+import satData from "./components/satData";
 
 
 function App() {
+  const [ sat, setSat ] = useState( satData );//sat used to compare changes in state. setStat is function will be passed
+
+  //both used in other components
+  const displaySats = [ ...new Set( satData.map( ( data ) => data.orbitType ) ) ];
+  //variable uses Set method to create new array holding unique elements
+  //used to create buttons
+  //prevents dupe els: 3 orb types: low/med/high converts into buttons
   return (
     <div>
       <Banner />
